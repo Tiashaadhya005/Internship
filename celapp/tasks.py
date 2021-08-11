b50593aa25c4bd5034bdb54e01fc6d8156d89bbf
Tiashaadhya005 <tiashaadhya03@gmail.com>
2021-08-11 19:52:43 +0530

    celery project

---
+++ celapp/tasks.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import , unicode_literals
+from celery import shared_task
+
+@shared_task
+def add(a,b):
+    return a+b
+
+@shared_task
+def mul(x, y):
+    return x * y
---
+++ sen/celery.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import , unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','sen.settings')
+BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+app = Celery('sen')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+app.conf.broker_url = BASE_REDIS_URL
+
+# this allows you to schedule items in the Django admin.
+app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
+#inside paranthesis lambda: settings.INSTALLED_APPS
+# @app.task(bind=True)
+# def debug_task(self):
+#     print('Request: {0!r}'.format(self.request))
