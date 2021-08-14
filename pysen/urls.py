f4414202a5104e18e9410fcc2261b1d0fd40db4b
Tiashaadhya005 <tiashaadhya03@gmail.com>
2021-08-14 12:34:25 +0530

    New upload

--- pysen/urls.py
+++ pysen/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path("",views.home, name='home'),
     path("addsen",views.addsen, name='addsen'),
     path("threads", views.threadss , name="threadss")
