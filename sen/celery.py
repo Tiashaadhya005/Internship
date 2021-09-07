4b1639286aa64b60a578f5c1eef5129b28207e00
Tiashaadhya005 <75745593+Tiashaadhya005@users.noreply.github.com>
2021-09-07 13:15:06 +0530

    Update celery.py

--- sen/celery.py
+++ sen/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','sen.settings')
-BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://redis:6379')
 app = Celery('sen')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
