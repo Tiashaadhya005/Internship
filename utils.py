65e57cd468ff8a4c1e792d4c6bb75645b9466c1a
Tiashaadhya005 <75745593+Tiashaadhya005@users.noreply.github.com>
2021-09-07 13:33:34 +0530

    updated after using docker

--- utils.py
+++ utils.py
@@ -1,13 +1,11 @@
 from pymongo import MongoClient
 
-client=MongoClient('localhost', 27017)
+uri ="mongodb://mongo/myproject"
+client= MongoClient(uri)
+
 mydb=client['myproject']
 
 def get_db():
-    #client=MongoClient('localhost', 27017)
-    #mydb= client['myproject']
-    #mycoll=mydb['collections']
-    #print(client)
     return mydb, client
 
 def get_coll():
