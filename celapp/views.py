2960b9efd115a429ff022d885a96d0a23217f326
Tiashaadhya005 <75745593+Tiashaadhya005@users.noreply.github.com>
2021-09-07 13:18:24 +0530

    celery implementation

--- celapp/views.py
+++ celapp/views.py
@@ -4,8 +4,11 @@ from .tasks import *
 # Create your views here.
 
 def celtask(request):
-    add.delay(3,5)
+    res1=add.delay(3467,5987)
+    res1.get()
     return render(request,"index.html")
 
 def celli(request):
+    res2=mul.delay(7890,7865)
+    res2.get()
     return redirect('/')
