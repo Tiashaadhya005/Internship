aeb9f72d82091c21ba35e14b1f29d048103d803e
Tiashaadhya005 <75745593+Tiashaadhya005@users.noreply.github.com>
2021-09-07 13:16:43 +0530

    celery implementation

--- celapp/urls.py
+++ celapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from .views import celli,celtask
 
 urlpatterns = [
-    path('/celtask', celtask, name='celtask'),
+    path('celtask', celtask, name='celtask'),
     path('celli', celli, name='celli')
 ]
